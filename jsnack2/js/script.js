cb73d7094a0e8340d0ba72cb8c5b7de900525c05
Mario <lombardomario01@gmail.com>
2021-10-28 16:27:05 +0200

    jsnack2

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,7 +1,60 @@
 // ---Jsnack-2---
+
 /*
 L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga.
 */
 
-console.log("Eccomi, sono Jsnack-2");
+/*
+
+--> DONE    1. Creo un array vuoto che dovrà contenere le due parole inserite dall'utente => const words = [];
+        --> DONE    1.2 Utilizzo un ciclo for per chiedere le parole 2 volte. => for
+        --> DONE    1.2 Utilizzo un push all'interno del ciclo per aggiungere le 2 parole all'interno dell'array => words.push();
+--> DONE    2. Scorro all'interno dell'array => for
+        --> DONE    2.1 Creo una variabile in cui assegno la lunghezza del primo valore dell'array fuori dal ciclo => let wordsLenMax = words[0].length;
+        --> DONE    2.2 Confronto la variabile con la lunghezza degli altri elementi dell'array per trovare la lunghezza maggiore => if(words[i].length >= wordsLenMax);
+                --> DONE    2.2.1 Se la lunghezza dell'elemento dell'array dovesse essere maggiore o uguale a wordsLen, assegno ad esso il valore di words[i] => wordsLenMax = words[i];
+--> DONE    3. Creo un altro ciclo uguale a quello di prima, solamente che devo trovare la lunghezza MINORE dell'elemento. => for
+        --> DONE 3.1 Creo un'altra variabile e la confronto con la lunghezza degli altri elementi dell'array per trovare la lunghezza minore => if(words[j].length < wordsLenMin);
+                --> DONE    3.3.1 Se la lunghezza dell'elemento dell'array dovesse essere minore a wordsLen, assegno ad esso il valore di words[i] => wordsLenMin = words[i];
+--> DONE    4 Stampo il risultato nell'html
+         --> DONE    4.1 Creo due variabili in cui assegno nella prima il valore minore, nella seconda invece assegno il valore maggiore 
+                --> DONE    4.1.1 => let maxWords = document.querySelector(".max_words");
+                --> DONE    4.1.2 => let minWords = document.querySelector(".min_words");
+        --> DONE    4.2 Appendo il risultato nelle variabili. => append
+*/
+
+const words = [];
+let maxWords = document.querySelector(".max_words");
+let minWords = document.querySelector(".min_words");
+
+for (let i = 1; i < 3; i++) {
+    let userWords = prompt(`Inserisci la parola n° ${i}`);
+    words.push(userWords);
+}
+
+console.log(words);
+
+let wordsLenMin = words[0].length;
+
+// Parola più corta
+for (let i = 0; i < words.length; i++) {
+    if(words[i].length < wordsLenMin) {
+        wordsLenMin = words[i];
+        console.log("la parola più corta è: ", words[i]);
+    }
+}
+
+minWords.append("La parola più corta è: ", wordsLenMin);
+
+let wordsLenMax = words[0].length;
+
+// Parola più lunga
+for(let j = 0; j < words.length; j++) {
+    if (words[j].length >= wordsLenMax) {
+        wordsLenMax = words[j];
+        console.log("la parola più lunga è: ", words[j]);
+    }
+}
+
+maxWords.append("La parola più lunga è: ", wordsLenMax);
