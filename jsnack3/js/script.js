4e46d807d0bce41abbbb2282e6a61222068a4404
Mario <lombardomario01@gmail.com>
2021-10-28 17:33:05 +0200

    Jsnack3 completed

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -1,7 +1,34 @@
 // ---Jsnack-3---
+
 /*
 Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.
 */
 
-console.log("Eccomi, sono Jsnack-3");
+/*
+
+--DONE    1. Creo un'array in cui conterrà i 10 numeri inseriti dall'utente => let contNum = [];
+--DONE  2. Creo un ciclo for che verrà eseguito 10 volte, in cui chiederà ogni giro che fa un numero all'utente. => prompt
+    --DONE  2.1 Utilizzo .push per riempire l'array ogni volta che l'utente inserisce il numero. => contNum.push[i];
+3. Creo un ciclo for in cui sommerò tutti i numeri inseriti dall'utente. => for
+    3.1 Creo una variabile "SUM" che conterrà la somma totale di tutti i numeri dell'array.
+    3.2 Sommo tutti i numeri tramite => sum += contNum[i];
+*/
+
+let contNum = [];
+let result = document.querySelector(".results");
+
+for (let i = 1; i < 11; i++) {
+    let userNum = parseInt(prompt(`Inserisci il numero: ${i}`));
+    contNum.push(userNum);
+}
+
+console.log(contNum);
+
+let sum = 0;
+
+for (let j = 0; j < contNum.length; j++) {
+    sum += contNum[j];
+}
+
+result.append("Somma dell'array = ", sum);
