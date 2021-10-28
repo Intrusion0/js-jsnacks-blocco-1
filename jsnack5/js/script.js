e7d10cb51c3636420c153f13b30f9344c1feca02
Mario <lombardomario01@gmail.com>
2021-10-28 18:13:41 +0200

    Jsnack5 Completed

--- jsnack5/js/script.js
+++ jsnack5/js/script.js
@@ -1,8 +1,27 @@
 // ---Jsnack-5---
+
 /*
 Crea un array vuoto.
 Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array.
 */
 
-console.log("Eccomi, sono Jsnack-5");
+/*
+
+--DONE  1.  Creo un array vuoto => let contNum = [];
+--DONE  2.  Creo un ciclo for in cui chiederò all'utente di inserire un numero => for - prompt
+3.  Creo una condizione per controllare se sia dispari. => if(numeroInserito % 2 !== 0)
+    3.1 Se è dispari lo inserisco all'interno dell'array => contNum.push
+
+*/
+
+let contNum = [];
+
+for(let i = 1; i <= 6; i++){
+    let userNum = parseInt(prompt(`Inserisci il numero: ${i}`));
+    if (userNum % 2 !== 0) {
+        contNum.push(userNum);
+    }
+    console.log(userNum);
+    console.log(contNum);
+}
