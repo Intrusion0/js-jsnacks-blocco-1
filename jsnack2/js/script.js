6cb29ea1b09557ccdd82378ce42903bdf3e54cd2
Mario <lombardomario01@gmail.com>
2021-10-28 17:14:06 +0200

    jsnack2 completed

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -5,7 +5,7 @@ L’utente inserisce due parole in successione, con due prompt.
 Il software stampa prima la parola più corta, poi la parola più lunga.
 */
 
-/*
+/*  METODO ALTERNATIVO
 
 --> DONE    1. Creo un array vuoto che dovrà contenere le due parole inserite dall'utente => const words = [];
         --> DONE    1.2 Utilizzo un ciclo for per chiedere le parole 2 volte. => for
@@ -24,37 +24,48 @@ Il software stampa prima la parola più corta, poi la parola più lunga.
         --> DONE    4.2 Appendo il risultato nelle variabili. => append
 */
 
-const words = [];
-let maxWords = document.querySelector(".max_words");
-let minWords = document.querySelector(".min_words");
+let firstwords = prompt("Inserisci la prima parola");
+let secondwords = prompt("Inserisci la seconda parola");
+let textResult = document.querySelector(".results");
 
-for (let i = 1; i < 3; i++) {
-    let userWords = prompt(`Inserisci la parola n° ${i}`);
-    words.push(userWords);
+if (firstwords.length > secondwords.length) {
+    textResult.append(secondwords + ' ' + firstwords);
+} else if (firstwords.length == secondwords.length) {
+    textResult.append('Le parole hanno la stessa lunghezza ' + secondwords + ' ' + firstwords);
+} else {
+    textResult.append(firstwords + ' ' + secondwords);
 }
 
-console.log(words);
 
-let wordsLenMin = words[0].length;
+// --METODO ALTERNATIVO (funzionante solo se inserisco la parola più lunga nel primo prompt);
 
-// Parola più corta
-for (let i = 0; i < words.length; i++) {
-    if(words[i].length < wordsLenMin) {
-        wordsLenMin = words[i];
-        console.log("la parola più corta è: ", words[i]);
-    }
-}
+// for (let i = 1; i < 3; i++) {
+//     let userWords = prompt(`Inserisci la parola n° ${i}`);
+//     words.push(userWords);
+// }
 
-minWords.append("La parola più corta è: ", wordsLenMin);
+// console.log(words);
 
-let wordsLenMax = words[0].length;
+// let wordsLenMin = words[0].length;
 
-// Parola più lunga
-for(let j = 0; j < words.length; j++) {
-    if (words[j].length >= wordsLenMax) {
-        wordsLenMax = words[j];
-        console.log("la parola più lunga è: ", words[j]);
-    }
-}
+// // Parola più corta
+// for (let i = 0; i < words.length; i++) {
+//     if(words[i].length < wordsLenMin) {
+//         wordsLenMin = words[i];
+//         console.log("la parola più corta è: ", words[i]);
+//     }
+// }
 
-maxWords.append("La parola più lunga è: ", wordsLenMax);
+// minWords.append("La parola più corta è: ", wordsLenMin);
+
+// let wordsLenMax = words[0].length;
+
+// // Parola più lunga
+// for(let j = 0; j < words.length; j++) {
+//     if (words[j].length >= wordsLenMax) {
+//         wordsLenMax = words[j];
+//         console.log("la parola più lunga è: ", words[j]);
+//     }
+// }
+
+// maxWords.append("La parola più lunga è: ", wordsLenMax);
